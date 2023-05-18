c023d570d2a1522ee2477cfad768a78848bb0877
yassine <keptsa@yahoo.fr>
2023-05-18 19:23:00 +0200

    ajout du champ section dans les data models

--- backend/autonhome/serre/models.py
+++ backend/autonhome/serre/models.py
@@ -9,8 +9,15 @@ class Sensor(models.Model):
         ('Luminosity', 'Luminosity'),
         # Add other sensor types if necessary
     )
+    SECTION_CHOICES = (
+        ('Air', 'Air'),
+        ('Soil', 'Soil'),
+        ('Water', 'Water'),
+        # Add other sensor types if necessary
+    )
 
     sensor_type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    section = models.CharField(max_length=15, choices=SECTION_CHOICES)
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
 
@@ -22,9 +29,14 @@ class Measure(models.Model):
     sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
     value = models.FloatField()
     timestamp = models.DateTimeField(default=timezone.now)
+    section = models.CharField(max_length=15, choices=Sensor.SECTION_CHOICES, blank=True)
 
     def __str__(self):
         return f"{self.sensor.name} - {self.value} - {self.timestamp}"
+
+    def save(self, *args, **kwargs):
+        self.section = self.sensor.section
+        super().save(*args, **kwargs)
 
 
 class CalendarEvent(models.Model):
